ce2fcb64ed33a365f315a3aed44f03631d8113d2
pazuzu2008 <kucherov.aleksey@gmail.com>
2026-03-13 16:22:00 +0200

    Add files via upload

--- api/proofread.js
+++ api/proofread.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        model: "llama3-70b-8192", 
+        model: "llama-3.3-70b-versatile", 
         messages: [{ role: "user", content: prompt }],
         temperature: 0.4
       })
